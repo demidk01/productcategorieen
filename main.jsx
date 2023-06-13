d2490d3f918d2b4809dc736c64b1f4907eb8459c
demidk01 <66522992+demidk01@users.noreply.github.com>
2023-06-13 10:51:16 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,7 +0,0 @@
-import "regenerator-runtime/runtime";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
